0c84004c3b226c77956eecb1fabe4626e79108f9
Fabio Lopes <fabioacl96@gmail.com>
2026-03-20 15:22:03 +0100

    Fix action icon bug

--- src/sample_preparation_widgets.py
+++ src/sample_preparation_widgets.py
@@ -1795,6 +1795,7 @@ class RegisterActionWidget(ipw.VBox):
         self.action_index = action_index
         self.instrument_permid = instrument_permid
         self.process_step_widget = process_step_widget
+        self.action_icon = ""
 
         global INSTRUMENT_COMPONENTS
 
